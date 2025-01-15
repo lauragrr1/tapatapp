532821f4eba6dcfad5be1637612fad8591aa0eeb
lauragrr1 <lalapato2000@gmail.com>
2025-01-15 13:11:46 +0100

    Update server.py

--- Prototip 1/server.py
+++ Prototip 1/server.py
@@ -1,41 +1,37 @@
 from flask import Flask, request, jsonify
 
-# Clase User
+
+
 class User:
-    def __init__(self, id, username, password, email):
-        self.id = id
-        self.username = username
-        self.password = password
-        self.email = email
+    def __init__(self, id, username, password, email=""):
+        self.id=id
+        self.username=username
+        self.password=password
+        self.email=email
 
     def __str__(self):
-        return "Id: " + str(self.id) + "Username: " + self.username
-    
+        return "Id:" + str(self.id) + " Username:" + self.username
+
 listUsers= [
-    User(1, "usuari1", "12345", "prova@gmail.com"),
-    User(2, "usuari2", "6789", "user@gmail.com"),
-    User(3, "usuari3", "0987", "admin@gmail.com"),
+    User(1,"usuari1", "12345", "prova@gmail.com"),
+    User(2,"user2", "123", "user2@proven.cat"),
+    User(3,"admin","12","admin@proven.cat"),
+    User(4,"admin2","12")
 ]
 
-for u in listUsers:
-    print(u)
-
-
-# Class Dao Users
-class DaoUsers:
+class DAOUsers:
     def __init__(self):
         self.users=listUsers
-
+    
     def getUserByUsername(self,username):
-        # Busca en la lista de usuarios
         for u in self.users:
             if u.username == username:
                 return u
-        return None  # Si no se encuentra, devuelve None
+        return None
 
-dao = DaoUsers()
+daoUser = DAOUsers()
 
-u=dao.getUserByUsername("usuari1")
+u=daoUser.getUserByUsername("usuari1")
 if(u):
     print(u)
 else:
@@ -48,4 +44,4 @@ def hello():
     return "Hello World"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(debug=True,host="0.0.0.0",port="10050")
