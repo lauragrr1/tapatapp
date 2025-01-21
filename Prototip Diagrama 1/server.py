ab6bbdd2f5937b7fef332459d26c51ecf168bd7e
Laura <klgrageda05@gmail.com>
2025-01-21 14:22:10 +0100

    server

--- Prototip Diagrama 1/server.py
+++ Prototip Diagrama 1/server.py
@@ -38,9 +38,5 @@ else:
 
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET'])
-def hello():
-    return "Hello World"
-
 if __name__ == '__main__':
      app.run(debug=True,host="0.0.0.0",port="10050")
