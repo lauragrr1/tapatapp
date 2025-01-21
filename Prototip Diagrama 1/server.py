e1f1bcda9b521c9865a49d5ca37eef19fbbc7f78
Laura <klgrageda05@gmail.com>
2025-01-21 14:40:00 +0100

    nm

--- Prototip Diagrama 1/server.py
+++ Prototip Diagrama 1/server.py
@@ -40,7 +40,9 @@ app = Flask(__name__)
 
 @app.route('/tapatapp/getuser', methods=['GET'])
 def getUser():
-    return "Im Laura Grageda"
+    n = request.args.get('username')
+    email = request.args.get('email')
+    return "Hello, Im " + n + "with email: " + email
 
 if __name__ == '__main__':
      app.run(debug=True,host="0.0.0.0",port="10050")
