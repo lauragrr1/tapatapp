e99e7f5aaac7dd743dead69c77c808bb6ac1b4e3
Laura <klgrageda05@gmail.com>
2025-01-29 13:29:20 +0100

    ssss

--- Prototip 1/server.py
+++ Prototip 1/server.py
@@ -30,13 +30,13 @@ class DAOUsers:
 
 daoUser = DAOUsers()
 
-'''
+
 u=daoUser.getUserByUsername("usuari1")
 if(u):
     print(u)
 else:
     print("Usuari no trobat")
-'''
+
 
 app = Flask(__name__)
 
