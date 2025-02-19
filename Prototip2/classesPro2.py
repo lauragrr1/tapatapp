8c8b4dfa9fa17727c9850b842a3af527c7cdab0d
Laura <klgrageda05@gmail.com>
2025-02-19 12:39:02 +0100

    jaja

--- Prototip2/classesPro2.py
+++ Prototip2/classesPro2.py
@@ -46,13 +46,11 @@ class Treatment:
         self.id = id
         self.name = name
 
-# Users
 users = [
     User(id=1, username="Mare", password="DFG123", email="mare@gmail.com"),
     User(id=2, username="Pare", password="POP098", email="pare@gmail.com")
 ]
 
-# Child
 children = [
     Child(id=1, child_name="Rocío Child", sleep_average=8, treatment_id=1, time=6),
     Child(id=2, child_name="Norman Child", sleep_average=10, treatment_id=2, time=6),
@@ -60,14 +58,12 @@ children = [
     Child(id=4, child_name="Nicole Child", sleep_average=10, treatment_id=4, time=4)
 ]
 
-# Taps
 taps = [
-    Tap(id=1, child_id=1, status_id=1, user_id=1, init="2025-01-06T19:42:00", end="2025-01-06T22:42:00"),
-    Tap(id=2, child_id=2, status_id=2, user_id=2, init="2025-01-11T21:31:00", end="2025-01-11T23:31:00"),
-    Tap(id=3, child_id=3, status_id=3, user_id=1, init="2025-01-10T22:30:00", end="2025-01-11T07:30:00"),
-    Tap(id=4, child_id=4, status_id=4, user_id=2, init="2025-01-15T20:55:00", end="2025-01-16T09:55:00")
+    Tap(id=1, child_id=1, status_id=2, user_id=1, init="2025-01-06T19:42:00", end="2025-01-06T22:42:00"),
+    Tap(id=2, child_id=2, status_id=3, user_id=2, init="2025-01-11T21:31:00", end="2025-01-11T23:31:00"),
+    Tap(id=3, child_id=3, status_id=1, user_id=1, init="2025-01-10T22:30:00", end="2025-01-11T07:30:00"),
+    Tap(id=4, child_id=4, status_id=1, user_id=2, init="2025-01-15T20:55:00", end="2025-01-16T09:55:00")
 ]
-
 
 relation_user_child = [
     {"user_id": 1, "child_id": 1, "rol_id": 2},
@@ -76,7 +72,6 @@ relation_user_child = [
     {"user_id": 2, "child_id": 2, "rol_id": 2}
 ]
 
-# Roles
 roles = [
     Role(id=1, type_rol='Admin'),
     Role(id=2, type_rol='Tutor/Mare/Pare'),
@@ -84,7 +79,6 @@ roles = [
     Role(id=4, type_rol='Seguiment')
 ]
 
-# Status
 statuses = [
     Status(id=1, name="sleep"),
     Status(id=2, name="awake"),
@@ -92,7 +86,6 @@ statuses = [
     Status(id=4, name="no_eyepatch")
 ]
 
-#Status
 treatments = [
     Treatment(id=1, name='Hour'),
     Treatment(id=2, name='percentage')
