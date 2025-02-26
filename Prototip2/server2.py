459a77f734a68684f0493454f980170a2560be3d
Laura <klgrageda05@gmail.com>
2025-02-26 12:31:30 +0100

    dd

--- Prototip2/server2.py
+++ Prototip2/server2.py
@@ -1,16 +1,8 @@
-import dadesServer as dades
-from dadesServer import User,Child,Tap,Status,Role,Treatment
+import dadesPro2 as dades
+from dadesPro2 import User,Child,Tap,Status,Role,Treatment
 from flask import Flask, jsonify, request
 
-# Exemple d'ús de la llista d'usuaris
-'''for x in dades.users:
-    print(x)
-
-# Exemple d'ús de la classe User
-a= User(id=1, username="Kurl", password="12345", email="prova2@gmail.com")
-print(a)'''
-
-############  DAOs  ############
+# DAOs  
 
 class UserDAO:
     def __init__(self):
