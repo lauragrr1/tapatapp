311e2753e6dc857967a9d379e2f95559011c82f2
Laura <klgrageda05@gmail.com>
2025-02-26 12:35:11 +0100

    ddd

--- Prototip2/server2.py
+++ Prototip2/server2.py
@@ -22,29 +22,22 @@ class ChildDAO:
         self.children = dades.children
 
     def get_all_children(self):
-        # Inicialitzar una llista buida per emmagatzemar els diccionaris dels fills
         children_dicts = []
         # Recórrer cada objecte 'child' en la llista 'self.children'
         for child in self.children:
-            # Convertir l'objecte 'child' en diccionari i afegir-lo a la llista
             children_dicts.append(child.__dict__)
         return children_dicts
 
     def get_children_by_user_id(self, user_id):
-        # Inicialitzar una llista buida per emmagatzemar els child_ids
         child_ids = []
         # Recórrer cada relació a la llista relation_user_child
         for rel in dades.relation_user_child:
             # Comprovar si el user_id de la relació coincideix amb el user_id donat
             if rel["user_id"] == user_id:
-                # Afegir el child_id a la llista child_ids
                 child_ids.append(rel["child_id"])
-        # Inicialitzar una llista buida per emmagatzemar els diccionaris dels fills
         children_dicts = []
-        # Recórrer cada objecte 'child' en la llista 'self.children'
         for child in self.children:
             # Comprovar si l'ID del child està dins de la llista child_ids
             if child.id in child_ids:
-                # Afegir el diccionari de l'objecte child a la llista
                 children_dicts.append(child.__dict__)
         return children_dicts
