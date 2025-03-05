4e07049557c3ed762b87a1337daf391a1db6b2e4
lauragrr1 <lalapato2000@gmail.com>
2025-03-05 02:32:51 +0100

    ss

--- Prototip2/dadesPro2.py
+++ Prototip2/dadesPro2.py
@@ -10,11 +10,12 @@ class User:
         return self.username + ":" + self.password + ":" + self.email
 
 users = [
-    User(id=1, username="mare", password="mare123", email="mare@gmail.com"),
-    User(id=2, username="pare", password="pare123", email="pare@gmail.com")
+    User(id=1, username="mare", password="12345", email="prova@gmail.com"),
+    User(id=2, username="pare", password="123", email="prova2@gmail.com")
 ]
 
-# Clase Child
+# Crear les classes Child, Tap, Role, Status i Treatment
+
 class Child:
     def __init__(self, id, child_name, sleep_average, treatment_id, time):
         self.id = id
@@ -23,12 +24,14 @@ class Child:
         self.treatment_id = treatment_id
         self.time = time
 
+    def __str__(self):
+        return "Child Id:" + str(self.id) + " Name:" + self.child_name
+
 children = [
-    Child(id=1, child_name="Rocío Child", sleep_average=8, treatment_id=1, time=6),
-    Child(id=2, child_name="Norman Child", sleep_average=10, treatment_id=2, time=6)
+    Child(id=1, child_name="Carol Child", sleep_average=8, treatment_id=1, time=6),
+    Child(id=2, child_name="Jaco Child", sleep_average=10, treatment_id=2, time=6)
 ]
 
-# Clase Tap
 class Tap:
     def __init__(self, id, child_id, status_id, user_id, init, end):
         self.id = id
@@ -37,62 +40,64 @@ class Tap:
         self.user_id = user_id
         self.init = init
         self.end = end
+    
+    def __str__(self):
+        return "Child Id:" + str(self.child_id) + " Status Id:" + str(self.status_id)
 
 taps = [
-    Tap(id=1, child_id=1, status_id=2, user_id=1, init="2025-01-06T19:42:00", end="2025-01-06T22:42:00"),
-    Tap(id=2, child_id=2, status_id=3, user_id=2, init="2025-01-11T21:31:00", end="2025-01-11T23:31:00")
-    
+    Tap(id=1, child_id=1, status_id=1, user_id=1, init="2024-12-18T19:42:43", end="2024-12-18T20:42:43"),
+    Tap(id=2, child_id=2, status_id=2, user_id=2, init="2024-12-18T21:42:43", end="2024-12-18T22:42:43")
 ]
 
-# Clase Status
-class Status:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
-    def __str__(self):
-        return self.id + ":" + self.name
-
-statuses = [
-    Status(id=1, name="sleep"),
-    Status(id=2, name="awake"),
-    Status(id=3, name="yes_eyepatch"),
-    Status(id=4, name="no_eyepatch")
+relation_user_child = [
+    {"user_id": 1, "child_id": 1, "rol_id": 1},
+    {"user_id": 1, "child_id": 1, "rol_id": 2},
+    {"user_id": 2, "child_id": 2, "rol_id": 1},
+    {"user_id": 2, "child_id": 2, "rol_id": 2}
 ]
 
-# Clase Role
 class Role:
     def __init__(self, id, type_rol):
         self.id = id
         self.type_rol = type_rol
 
     def __str__(self):
-        return self.id + ":" + self.type_rol
+        return "Role Id: " + str(self.id) + " Type Rol: " + self.type_rol
 
 roles = [
     Role(id=1, type_rol='Admin'),
-    Role(id=2, type_rol='Tutor/Mare/Pare'),
+    Role(id=2, type_rol='Tutor Mare Pare'),
     Role(id=3, type_rol='Cuidador'),
     Role(id=4, type_rol='Seguiment')
 ]
 
-# Clase Treatment
-class Treatment:
-    def __init__(self, id, name):
+class Status:
+    def __init__(self, id, child_id, name, status_id):
         self.id = id
+        self.child_id = child_id
+        self.name = name
+        self.status_id = status_id
+
+    def __str__(self):
+        return "Status Id: " + str(self.status_id) + " Name: " + self.name + " Child Id: " + str(self.child_id)
+    
+statuses = [
+    Status(id=1, child_id=1, name="sleep", status_id=1),
+    Status(id=2, child_id=1, name="awake", status_id=2),
+    Status(id=3, child_id=2, name="yes_eyepatch", status_id=3),
+    Status(id=4, child_id=2, name="no_eyepatch", status_id=4)
+]
+
+class Treatment:
+    def __init__(self, id, child_id, name):
+        self.id = id
+        self.child_id = child_id
         self.name = name
 
     def __str__(self):
-        return self.id + ":" + self.name
+        return "Treatment Id: " + str(self.id) + " Name: " + self.name
 
 treatments = [
-    Treatment(id=1, name='Hour'),
-    Treatment(id=2, name='percentage')
-]
-
-relation_user_child = [
-    {"user_id": 1, "child_id": 1, "rol_id": 2},
-    {"user_id": 1, "child_id": 1, "rol_id": 2},
-    {"user_id": 2, "child_id": 2, "rol_id": 2},
-    {"user_id": 2, "child_id": 2, "rol_id": 2}
+    Treatment(id=1, child_id=1, name='Hour'),
+    Treatment(id=2, child_id=2, name='Percentage')
 ]
