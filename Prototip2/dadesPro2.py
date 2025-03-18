b2e617c89737deebd90863e1c9c40234b2a4e323
Laura <klgrageda05@gmail.com>
2025-03-18 14:23:53 +0100

    pro2

--- Prototip2/dadesPro2.py
+++ Prototip2/dadesPro2.py
@@ -1,5 +1,7 @@
-# Dades d'exemple amb List 
-# Clase User 
+# Clase User
+import requests
+
+
 class User:
     def __init__(self, id, username, password, email):
         self.id = id
@@ -10,7 +12,13 @@ class User:
     def __str__(self):
         return self.username + ":" + self.password + ":" + self.email
 
-# Clase Child
+users = [
+    User(id=1, username="mare", password="12345", email="prova@gmail.com"),
+    User(id=2, username="pare", password="123", email="prova2@gmail.com")
+]
+
+# Crear les classes Child, Tap, Role, Status i Treatment
+
 class Child:
     def __init__(self, id, child_name, sleep_average, treatment_id, time):
         self.id = id
@@ -19,7 +27,14 @@ class Child:
         self.treatment_id = treatment_id
         self.time = time
 
-# Clase Tap
+    def __str__(self):
+        return "Child Id:" + str(self.id) + " Name:" + self.child_name
+
+children = [
+    Child(id=1, child_name="Carol Child", sleep_average=8, treatment_id=1, time=6),
+    Child(id=2, child_name="Jaco Child", sleep_average=10, treatment_id=2, time=6)
+]
+
 class Tap:
     def __init__(self, id, child_id, status_id, user_id, init, end):
         self.id = id
@@ -28,36 +43,9 @@ class Tap:
         self.user_id = user_id
         self.init = init
         self.end = end
-
-# Clase Status
-class Status:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
-# Clase Role
-class Role:
-    def __init__(self, id, type_rol):
-        self.id = id
-        self.type_rol = type_rol
-
-# Clase Treatment
-class Treatment:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
-users = [
-    User(id=1, username="mare", password="12345", email="prova@gmail.com"),
-    User(id=2, username="pare", password="123", email="prova2@gmail.com")
-]
-
-# Crear les classes Child, Tap, Role, Status i Treatment
-
-children = [
-    Child(id=1, child_name="Carol Child", sleep_average=8, treatment_id=1, time=6),
-    Child(id=2, child_name="Jaco Child", sleep_average=10, treatment_id=2, time=6)
-]
+    
+    def __str__(self):
+        return "Child Id:" + str(self.child_id) + " Status Id:" + str(self.status_id)
 
 taps = [
     Tap(id=1, child_id=1, status_id=1, user_id=1, init="2024-12-18T19:42:43", end="2024-12-18T20:42:43"),
@@ -66,11 +54,18 @@ taps = [
 
 relation_user_child = [
     {"user_id": 1, "child_id": 1, "rol_id": 1},
-    {"user_id": 1, "child_id": 2, "rol_id": 1},
     {"user_id": 1, "child_id": 1, "rol_id": 2},
     {"user_id": 2, "child_id": 2, "rol_id": 1},
     {"user_id": 2, "child_id": 2, "rol_id": 2}
 ]
+
+class Role:
+    def __init__(self, id, type_rol):
+        self.id = id
+        self.type_rol = type_rol
+
+    def __str__(self):
+        return "Role Id: " + str(self.id) + " Type Rol: " + self.type_rol
 
 roles = [
     Role(id=1, type_rol='Admin'),
@@ -79,14 +74,96 @@ roles = [
     Role(id=4, type_rol='Seguiment')
 ]
 
+class Status:
+    def __init__(self, id, child_id, name, status_id):
+        self.id = id
+        self.child_id = child_id
+        self.name = name
+        self.status_id = status_id
+
+    def __str__(self):
+        return "Status Id: " + str(self.status_id) + " Name: " + self.name + " Child Id: " + str(self.child_id)
+    
 statuses = [
-    Status(id=1, name="sleep"),
-    Status(id=2, name="awake"),
-    Status(id=3, name="yes_eyepatch"),
-    Status(id=4, name="no_eyepatch")
+    Status(id=1, child_id=1, name="sleep", status_id=1),
+    Status(id=2, child_id=1, name="awake", status_id=2),
+    Status(id=3, child_id=2, name="yes_eyepatch", status_id=3),
+    Status(id=4, child_id=2, name="no_eyepatch", status_id=4)
 ]
 
+class Treatment:
+    def __init__(self, id, child_id, name):
+        self.id = id
+        self.child_id = child_id
+        self.name = name
+
+    def __str__(self):
+        return "Treatment Id: " + str(self.id) + " Name: " + self.name
+
 treatments = [
-    Treatment(id=1, name='Hour'),
-    Treatment(id=2, name='percentage')
+    Treatment(id=1, child_id=1, name='Hour'),
+    Treatment(id=2, child_id=2, name='Percentage')
 ]
+
+# Funció per autenticar a l'usuari a través del servidor Flask
+def authenticate_user(username, password):
+    response = requests.post('http://localhost:10050/Prototip2', json={"username": username, "password": password})
+    return response
+
+# Funció per mostrar la informació de l'usuari
+def show_user_info(user):
+    print(f"Username: {user['username']}")
+    print(f"Email: {user['email']}")
+    print(f"Password: {user['password']}")
+
+# Funció per llistar els nens
+def list_children(children):
+    print("Child List:")
+    for child in children:
+        print(f"ID: {child['id']}, Nom: {child['child_name']}, Mitjana de son: {child['sleep_average']}, Tractament: {child['treatment_id']}, Temps: {child['time']}")
+        
+
+# Funció principal
+def main():
+    print(" ")
+    username = input("Introdueix el teu nom d'usuari: ")
+    password = input("Introdueix la teva contrasenya: ")
+    
+    response = authenticate_user(username, password)
+    
+    if response.status_code == 200:
+        data = response.json()
+        print(" ")
+        print(f"Welcome, {data['user_info']['username']}!")
+        print("Tria una de les opcions")
+        print(" ")
+
+        while True:
+            print("Selecciona una opció:")
+            print("1. User info")
+            print("2. List Child")
+            print("3. Salir")
+            
+            option = input("Selecciona una opció: ")
+            
+            if option == "1":
+                print(" ")
+                show_user_info(data['user_info'])
+                print(" ")
+            elif option == "2":
+                print(" ")
+                list_children(data['children'])
+                print(" ")
+            elif option == "3":
+                print(" ")
+                print("Sortint...")
+                break
+            else:
+                print(" ")
+                print("Opció no vàlida.")
+                print(" ")
+    else:
+        print("Usuari o contrasenya incorrectes.")
+
+if __name__ == "__main__":
+    main()
