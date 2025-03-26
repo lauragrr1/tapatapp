9d81d565e5ad4ab4b74035b3455eb4fd30969587
Laura <klgrageda05@gmail.com>
2025-03-26 13:19:33 +0100

    cl

---
+++ Prototip3/client.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from serverP3 import DAOUser
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    identifier = data.get('identifier')
+    password = data.get('password')
+    user = DAOUser.login(identifier, password)
+    if user:
+        return jsonify({
+            'id': user.id,
+            'username': user.username,
+            'email': user.email
+        }), 200
+    else:
+        return jsonify({'error': 'Invalid credentials'}), 401
+
+if __name__ == '__main__':
+    app.run(debug=True)
