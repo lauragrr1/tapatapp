ef35a7c872b4f94d5878d84c21bfacf4de36afa1
Laura <klgrageda05@gmail.com>
2025-03-26 13:18:34 +0100

    sv3

--- Prototip3/serverP3.py
+++ Prototip3/serverP3.py
@@ -1,42 +1,108 @@
-from dadesPro2 import users  # Importamos la lista de usuarios
-from flask import Flask, jsonify, request
+# Dades d'exemple amb List 
+# Clase User 
+class User:
+    def __init__(self, id, username, password, email):
+        self.id = id
+        self.username = username
+        self.password = password
+        self.email = email
+    
+    def __str__(self):
+        return self.username + ":" + self.password + ":" + self.email
 
-app = Flask(__name__)
+# Clase Child
+class Child:
+    def __init__(self, id, child_name, sleep_average, treatment_id, time):
+        self.id = id
+        self.child_name = child_name
+        self.sleep_average = sleep_average
+        self.treatment_id = treatment_id
+        self.time = time
 
-# Datos
+# Clase Tap
+class Tap:
+    def __init__(self, id, child_id, status_id, user_id, init, end):
+        self.id = id
+        self.child_id = child_id
+        self.status_id = status_id
+        self.user_id = user_id
+        self.init = init
+        self.end = end
+
+# Clase Status
+class Status:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+# Clase Role
+class Role:
+    def __init__(self, id, type_rol):
+        self.id = id
+        self.type_rol = type_rol
+
+# Clase Treatment
+class Treatment:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+
 users = [
-    {"id": 1, "username": "mare", "email": "mare@gmail.com"},
-    {"id": 2, "username": "pare", "email": "pare@gmail.com"}
+    User(id=1, username="mare", password="12345", email="prova@gmail.com"),
+    User(id=2, username="pare", password="123", email="prova2@gmail.com")
 ]
+
+# Crear les classes Child, Tap, Role, Status i Treatment
 
 children = [
-    {"id": 1, "name": "Laura Child", "sleep_average": 8, "treatment": "Hour", "time": 6},
-    {"id": 2, "name": "Christian Child", "sleep_average": 6, "treatment": "percentage", "time": 4}
+    Child(id=1, child_name="Carol Child", sleep_average=8, treatment_id=1, time=6),
+    Child(id=2, child_name="Jaco Child", sleep_average=10, treatment_id=2, time=6)
 ]
 
+taps = [
+    Tap(id=1, child_id=1, status_id=1, user_id=1, init="2024-12-18T19:42:43", end="2024-12-18T20:42:43"),
+    Tap(id=2, child_id=2, status_id=2, user_id=2, init="2024-12-18T21:42:43", end="2024-12-18T22:42:43")
+]
 
-# Ruta para obtener un usuario por nombre de usuario
-@app.route('/prototip2/getuser', methods=['GET'])
-def get_user():
-    username = request.args.get('username')
-    user = next((u for u in users if u["username"] == username), None)
-    if user:
-        return jsonify(user), 200
-    else:
-        return jsonify({"error": "Usuari no trobat"}), 404
+relation_user_child = [
+    {"user_id": 1, "child_id": 1, "rol_id": 1},
+    {"user_id": 1, "child_id": 2, "rol_id": 1},
+    {"user_id": 1, "child_id": 1, "rol_id": 2},
+    {"user_id": 2, "child_id": 2, "rol_id": 1},
+    {"user_id": 2, "child_id": 2, "rol_id": 2}
+]
 
+roles = [
+    Role(id=1, type_rol='Admin'),
+    Role(id=2, type_rol='Tutor Mare Pare'),
+    Role(id=3, type_rol='Cuidador'),
+    Role(id=4, type_rol='Seguiment')
+]
 
-# Ruta para obtener los niños asociados a un usuario (por simplicidad, asumimos todos los niños están relacionados con cualquier usuario)
-@app.route('/prototip2/getchildren/<username>', methods=['GET'])
-def get_children(username):
-    print(f"Rebuda petició per obtenir nens de l'usuari: {username}")  # Depuració
-    user = next((u for u in users if u["username"] == username), None)
-    if user:
-        print(f"Usuari trobat: {user}")  # Depuració
-        return jsonify(children), 200
-    else:
-        print("Usuari no trobat")  # Depuració
-        return jsonify({"error": "No children found"}), 404
+statuses = [
+    Status(id=1, name="sleep"),
+    Status(id=2, name="awake"),
+    Status(id=3, name="yes_eyepatch"),
+    Status(id=4, name="no_eyepatch")
+]
 
-if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port="10050")
+treatments = [
+    Treatment(id=1, name='Hour'),
+    Treatment(id=2, name='percentage')
+]
+
+class DAOUser:
+    @staticmethod
+    def login(identifier, password):
+        for user in users:
+            if (user.username == identifier or user.email == identifier) and user.password == password:
+                return user
+        return None
+
+class DAOChild:
+    @staticmethod
+    def get_children_by_user_id(user_id):
+        child_ids = [relation['child_id'] for relation in relation_user_child if relation['user_id'] == user_id]
+        return [child for child in children if child.id in child_ids]
