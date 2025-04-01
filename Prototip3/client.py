16c9cf6e051d626b652cdf7313a316c66b63a2be
Laura <klgrageda05@gmail.com>
2025-04-01 13:49:19 +0200

    apprun

--- Prototip3/client.py
+++ Prototip3/client.py
@@ -19,4 +19,4 @@ def login():
         return jsonify({'error': 'Invalid credentials'}), 401
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port="10050")
